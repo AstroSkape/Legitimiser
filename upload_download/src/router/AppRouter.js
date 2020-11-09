49fdd0ba0a1fcd4353786b4ec0f5bd986cf22bcc
shashank-madan <52149707+shashank-madan@users.noreply.github.com>
2020-11-09 11:30:48 +0530

    Add files via upload

---
+++ upload_download/src/router/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from '../components/App';
+import Header from '../components/Header';
+import FilesList from '../components/FilesList';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <div className="main-content">
+        <Switch>
+          <Route component={App} path="/" exact={true} />
+        </Switch>
+      </div>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
